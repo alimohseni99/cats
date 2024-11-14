33b0d315b9a7cbaf5247ff58041d3164cc524d0f
Ali Mohseni <ali.mohseni@appliedtechnology.se>
2024-11-14 09:18:32 +0100

    added input field and button to add cat

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,9 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div>
-      <h1>Hello World</h1>
+      <h1>Add a cat to the list! </h1>
+      <input type="text" />
+      <button>add Cat</button>
     </div>
   );
 }
